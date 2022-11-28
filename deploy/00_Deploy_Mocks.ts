c9186c656bff346efd70fc74779658e8a6feaa0e
0xbeny <55846654+0xbeny@users.noreply.github.com>
2022-11-28 15:54:52 +0330

    Initial commit

---
+++ deploy/00_Deploy_Mocks.ts
@@ -0,0 +1,7 @@
+import { DeployFunction } from "hardhat-deploy/types"
+import { getNamedAccounts, deployments, network } from "hardhat"
+
+const deployFunction: DeployFunction = async () => {}
+
+export default deployFunction
+deployFunction.tags = [`all`]
